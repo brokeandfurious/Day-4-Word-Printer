f51dc97b3a6d1ab0539704c7051c18964ca6b038
brokeandfurious <forumbomb@gmail.com>
2017-11-05 21:13:46 -0500

    Initial Commit

---
+++ Day 4 - Word Printer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Day 4 - Word Printer
+//
+//  Created by Murat Ekrem Kolcalar on 11/5/17.
+//  Copyright © 2017 mumu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
